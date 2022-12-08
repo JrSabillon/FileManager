70e4c8c32ae0a59b867fd4daf3750c0efb222846
JrSabillon <wilmersabillon25@gmail.com>
2022-12-07 23:54:51 -0600

    terminando pantalla de auditorias

--- SyT_FileManager/Controllers/BitacoraController.cs
+++ SyT_FileManager/Controllers/BitacoraController.cs
@@ -5,19 +5,33 @@ using System.Web;
 using System.Web.Mvc;
 using SyT_FileManager.AppCode;
 using SyT_FileManager.DataAccess;
-using SyT_FileManager.Models;
+using SyT_FileManager.Models.POCO;
 using PagedList;
 
 namespace SyT_FileManager.Controllers
 {
+    [AuthorizationHandler]
+    [ExceptionHandler]
     public class BitacoraController : Controller
     {
         BitacoraAccess BitacoraAccess;
         // GET: Bitacora
-        public ActionResult Index(int? page)
+        public ActionResult Index(int? page, BitacoraAuditoriaBusqueda busqueda)
         {
             BitacoraAccess = new BitacoraAccess();
             var model = BitacoraAccess.GetAll();
+
+            ViewBag.SearchDate = busqueda.SearchDate;
+            ViewBag.SearchActionType = busqueda.SearchActionType;
+            ViewBag.Accion = new SelectList(model.Select(x => x.Accion).Distinct());
+            ViewBag.SelectedAccion = busqueda.Accion;
+            ViewBag.FechaInicio = busqueda.FechaInicio;
+            ViewBag.FechaFin = busqueda.FechaFin;
+
+            if (busqueda.SearchDate && busqueda.FechaInicio.HasValue && busqueda.FechaFin.HasValue)
+                model = model.Where(x => x.Fecha >= busqueda.FechaInicio && x.Fecha <= busqueda.FechaFin).ToList();
+            if (busqueda.SearchDate && !string.IsNullOrEmpty(busqueda.Accion))
+                model = model.Where(x => x.Accion == busqueda.Accion).ToList();
 
             int pageSize = Constants.PaginationSize;
             int pageNumber = (page ?? 1);
---
+++ SyT_FileManager/Models/POCO/BitacoraAuditoriaBusqueda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SyT_FileManager.Models.POCO
+{
+    public class BitacoraAuditoriaBusqueda
+    {
+        public bool SearchDate { get; set; } = true;
+        public bool SearchActionType { get; set; } = true;
+        public DateTime? FechaInicio { get; set; }
+        public DateTime? FechaFin { get; set; }
+        public string Accion { get; set; }
+    }
+}
